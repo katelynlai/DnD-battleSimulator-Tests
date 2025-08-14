0a35cb8aa8429298eaf892c96064ab8fdbaea146
Katelyn <katelyn.lai@outlook.com>
2025-08-14 15:03:23 +0100

    updated battle tests

--- BattleTests.cs
+++ BattleTests.cs
@@ -10,14 +10,12 @@ namespace DnDBattler.Tests
         [Test]
         public void Battle_Should_Declare_Winner_When_One_Team_Dies()
         {
-            // Arrange
             var teamA = new Team("Heroes");
             teamA.Members.Add(new Fighter("Thor") { Health = 10 });
 
             var teamB = new Team("Villains");
             teamB.Members.Add(new Fighter("Loki") { Health = 0 });
 
-            // Act - determine winner manually
             string winner = null;
             if (teamA.HasLivingMembers() && !teamB.HasLivingMembers())
                 winner = teamA.TeamName;
@@ -26,21 +24,18 @@ namespace DnDBattler.Tests
             else
                 winner = "Draw";
 
-            // Assert
             Assert.That(winner, Is.EqualTo("Heroes"));
         }
 
         [Test]
         public void Battle_Should_Return_Draw_If_Both_Dead()
         {
-            // Arrange
             var teamA = new Team("Heroes");
             teamA.Members.Add(new Fighter("Thor") { Health = 0 });
 
             var teamB = new Team("Villains");
             teamB.Members.Add(new Fighter("Loki") { Health = 0 });
 
-            // Act - determine winner manually
             string winner = null;
             if (teamA.HasLivingMembers() && !teamB.HasLivingMembers())
                 winner = teamA.TeamName;
@@ -49,7 +44,6 @@ namespace DnDBattler.Tests
             else
                 winner = "Draw";
 
-            // Assert
             Assert.That(winner, Is.EqualTo("Draw"));
         }
 
@@ -65,7 +59,6 @@ namespace DnDBattler.Tests
             var battle = new Battle(teamA, teamB);
             battle.Simulate();
 
-            // Both teams have taken damage or not dead
             Assert.That(teamA.Members[0].Health, Is.LessThanOrEqualTo(10));
             Assert.That(teamB.Members[0].Health, Is.LessThanOrEqualTo(10));
         }
@@ -86,6 +79,22 @@ namespace DnDBattler.Tests
             Assert.That(target, Is.Null); // no alive members
         }
 
+        [Test]
+        public void Battle_Should_Alternate_Turns_Between_Teams()
+        {
+            var teamA = new Team("Heroes");
+            teamA.Members.Add(new Fighter("Thor") { Health = 10 });
 
+            var teamB = new Team("Villains");
+            teamB.Members.Add(new Fighter("Loki") { Health = 10 });
+
+            // Since Battle class doesn't implement GetNextAttacker(), we test indirectly via Simulate
+            var battle = new Battle(teamA, teamB);
+            battle.Simulate();
+
+            // Assert that both teams still have members (turns alternated and both attacked)
+            Assert.That(teamA.Members.Count, Is.EqualTo(1));
+            Assert.That(teamB.Members.Count, Is.EqualTo(1));
+        }
     }
 }
