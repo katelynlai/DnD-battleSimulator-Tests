b164c5c73a4eb29060f99e2d983e5fd6c4d89482
Katelyn <katelyn.lai@outlook.com>
2025-08-14 12:10:06 +0100

    added more tests for team

--- TeamTests.cs
+++ TeamTests.cs
@@ -17,6 +17,18 @@ namespace DnDBattler.Tests
             Assert.That(team.Members.Count, Is.EqualTo(3));
         }
 
+
+        [Test]
+        public void HasLivingMembers_Should_Return_True_If_Any_Alive()
+        {
+            var team = new Team("Wolves");
+            team.Members.Add(new Fighter("Conan") { Health = 0 });
+            team.Members.Add(new Wizard("Gandalf") { Health = 5 });
+            team.Members.Add(new Cleric("Sophia") { Health = 0 });
+
+            Assert.That(team.HasLivingMembers(), Is.True);
+        }
+
         [Test]
         public void IsAlive_Should_ReturnFalse_When_AllMembersDead()
         {
@@ -25,6 +37,31 @@ namespace DnDBattler.Tests
             team.Members.Add(fighter);
 
             Assert.That(team.HasLivingMembers(), Is.False);
+        }
+        
+        [Test]
+        public void GetRandomAliveMember_Should_Return_Only_Alive()
+        {
+            var team = new Team("Dragons");
+            team.Members.Add(new Fighter("Thor") { Health = 0 });
+            var wizard = new Wizard("Merlin") { Health = 5 };
+            team.Members.Add(wizard);
+            team.Members.Add(new Cleric("Angela") { Health = 0 });
+
+            var member = team.GetRandomAliveMember();
+            Assert.That(member, Is.EqualTo(wizard));
+        }
+
+        [Test]
+        public void GetRandomAliveMember_Should_Return_Null_When_No_Living()
+        {
+            var team = new Team("Dragons");
+            team.Members.Add(new Fighter("Thor") { Health = 0 });
+            team.Members.Add(new Wizard("Merlin") { Health = 0 });
+            team.Members.Add(new Cleric("Angela") { Health = 0 });
+
+            var member = team.GetRandomAliveMember();
+            Assert.That(member, Is.Null);
         }
     }
 }
