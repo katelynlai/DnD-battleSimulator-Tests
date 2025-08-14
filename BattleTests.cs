ed32d9a28eac2e9ad3621b0ad6cfe9bac0a26ad6
Katelyn <katelyn.lai@outlook.com>
2025-08-14 12:49:07 +0100

    added more battle tests

--- BattleTests.cs
+++ BattleTests.cs
@@ -52,5 +52,40 @@ namespace DnDBattler.Tests
             // Assert
             Assert.That(winner, Is.EqualTo("Draw"));
         }
+
+        [Test]
+        public void Battle_Should_Damage_Both_Teams()
+        {
+            var teamA = new Team("Heroes");
+            teamA.Members.Add(new Fighter("Thor") { Health = 10 });
+
+            var teamB = new Team("Villains");
+            teamB.Members.Add(new Fighter("Loki") { Health = 10 });
+
+            var battle = new Battle(teamA, teamB);
+            battle.Simulate();
+
+            // Both teams have taken damage or not dead
+            Assert.That(teamA.Members[0].Health, Is.LessThanOrEqualTo(10));
+            Assert.That(teamB.Members[0].Health, Is.LessThanOrEqualTo(10));
+        }
+
+        [Test]
+        public void Battle_Should_Only_Attack_Alive_Characters()
+        {
+            var teamA = new Team("Heroes");
+            var fighter = new Fighter("Thor") { Health = 0 };
+            teamA.Members.Add(fighter);
+
+            var teamB = new Team("Villains");
+            teamB.Members.Add(new Fighter("Loki") { Health = 10 });
+
+            var battle = new Battle(teamA, teamB);
+            var target = teamA.GetRandomAliveMember();
+
+            Assert.That(target, Is.Null); // no alive members
+        }
+
+
     }
 }
